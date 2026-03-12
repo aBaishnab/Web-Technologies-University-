b46a9d2a3f8c082094c6c310241273ff183514d5
aBaishnab <a.baishnab17@gmail.com>
2026-03-12 22:43:23 +0600

    Updated

--- Lab Exam/JS/script.js
+++ Lab Exam/JS/script.js
@@ -1,7 +1,7 @@
 function appendToDisplay(input){
     const display = document.getElementById("display");
 
-    if(display.value =="Invalid" || display.value =="undefined" || display.value =="Infinity" || display.value =="NaN"){
+    if(display.value =="Invalid" || display.value =="undefined" || display.value.includes("Infinity") || display.value =="NaN"){
         display.value = "";
     }
     display.value += input;
@@ -10,36 +10,37 @@ function appendToDisplay(input){
 
 function clearDisplay(){
     const display = document.getElementById("display");
-
      display.value = "";
 }
 
 function remove(){
     const display = document.getElementById("display");
 
-    if(display.value =="Invalid" || display.value =="undefined" || display.value =="Infinity" || display.value =="NaN"){
+    if(display.value =="Invalid" || display.value =="undefined" || display.value.includes("Infinity")|| display.value =="NaN"){
         display.value ="";
-        return;
     }
     display.value = display.value.slice(0, -1);
 }
 
 function calculate(){
     const display = document.getElementById("display");
-
-    try{
-        if(display.value.includes("**")){
-            display.value = "Invalid";
-            return;
-        }
-        else if(display.value.includes("//")){
-            display.value = "Invalid";
-            return;
-        }
-
-        display.value = eval(display.value);
+    if(display.value =="Invalid" || display.value =="undefined" || display.value.includes("Infinity") || display.value =="NaN"){
+        display.value = "";
     }
-    catch(error){
-        display.value ="Invalid";
+    else{
+        try{
+            if(display.value.includes("**")){
+                display.value = "Invalid";
+            }
+            else if(display.value.includes("//")){
+                display.value = "Invalid";
+            }
+
+            display.value = eval(display.value);
+        }
+        catch(error){
+            display.value ="Invalid";
+        }
     }
+    
 }  
